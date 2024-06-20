eafe7aa9d5c5205ddf42a66c953ff33dcfa15758
shourya-r <rajputshourya7@gmail.com>
2024-06-20 22:45:29 +0530

    Added tests with first letter as a digit and symbol

--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -7,3 +7,15 @@ test("abcd returns Abcd", () => {
 test("ABCD return ABCD", () => {
   expect(capitalize("ABCD")).toBe("ABCD");
 });
+
+test("aBCeXY returns ABCeXY", () => {
+  expect(capitalize("aBCeXY")).toBe("ABCeXY");
+});
+
+test("1abCd returns 1abCd", () => {
+  expect(capitalize("1abCd")).toBe("1abCd");
+});
+
+test("@abCd returns @abCd", () => {
+  expect(capitalize("@abCd")).toBe("@abCd");
+});
