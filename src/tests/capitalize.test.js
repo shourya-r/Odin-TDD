f0490a1d8100fc59ab02709b8b046cba03cf8ffc
shourya-r <rajputshourya7@gmail.com>
2024-06-20 22:39:06 +0530

    Added a new test with all capital letters

--- src/tests/capitalize.test.js
+++ src/tests/capitalize.test.js
@@ -1,3 +1,9 @@
+import { capitalize } from "../scripts/capitalize";
+
 test("abcd returns Abcd", () => {
   expect(capitalize("abcd")).toBe("Abcd");
+});
+
+test("ABCD return ABCD", () => {
+  expect(capitalize("ABCD")).toBe("ABCD");
 });
