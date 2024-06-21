8855c4ba946abda5b43f38aaf8b068934e114946
shourya-r <rajputshourya7@gmail.com>
2024-06-21 22:41:26 +0530

    Added checks to check for type

--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -1,14 +1,18 @@
 const calculator = {
   add(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") return undefined;
     return a + b;
   },
   subtract(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") return undefined;
     return a - b;
   },
   multiply(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") return undefined;
     return a * b;
   },
   divide(a, b) {
+    if (typeof a !== "number" || typeof b !== "number") return undefined;
     const x = a / b;
     let decimalPlaces;
     if (Math.floor(x) === x) decimalPlaces = 0;
