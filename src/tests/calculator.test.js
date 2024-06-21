69c56f0c6814c11ea478776f8d7e3708a316d9b5
shourya-r <rajputshourya7@gmail.com>
2024-06-21 22:09:05 +0530

    Added test for subtract function

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -3,3 +3,7 @@ import { calculator } from "../scripts/calculator";
 test("add(2,4) returns 6", () => {
   expect(calculator.add(2, 4)).toBe(6);
 });
+
+test("subtract(8,3) returns 5", () => {
+  expect(calculator.subtract(8, 3)).toBe(5);
+});
