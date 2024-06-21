81df4e3d69e1853e399a9b885afa43e04e07deb8
shourya-r <rajputshourya7@gmail.com>
2024-06-21 22:19:40 +0530

    Added tests for negative numbers

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -15,3 +15,19 @@ test("multiply(7,9) returns 63", () => {
 test("divide(9,2) returns 4.5", () => {
   expect(calculator.divide(9, 2)).toBe(4.5);
 });
+
+test("add(-2,4) returns 2", () => {
+  expect(calculator.add(-2, 4)).toBe(2);
+});
+
+test("subtract(3,-3) returns 11", () => {
+  expect(calculator.subtract(3, -3)).toBe(6);
+});
+
+test("multiply(-6,3) returns -18", () => {
+  expect(calculator.multiply(6, -3)).toBe(-18);
+});
+
+test("divide(8,-2) returns -4", () => {
+  expect(calculator.divide(8, -2)).toBe(-4);
+});
