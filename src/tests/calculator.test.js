8500ca54ba756418dc07dd70319cdad9c1a5bcfc
shourya-r <rajputshourya7@gmail.com>
2024-06-21 22:15:15 +0530

    Added test for divide function

--- src/tests/calculator.test.js
+++ src/tests/calculator.test.js
@@ -11,3 +11,7 @@ test("subtract(8,3) returns 5", () => {
 test("multiply(7,9) returns 63", () => {
   expect(calculator.multiply(7, 9)).toBe(63);
 });
+
+test("divide(9,2) returns 4.5", () => {
+  expect(calculator.divide(9, 2)).toBe(4.5);
+});
