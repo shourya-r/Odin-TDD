6dd1b62d131b1d9bc723b4c07451c497cf1a5026
shourya-r <rajputshourya7@gmail.com>
2024-06-21 21:51:29 +0530

    Added second test with a different string

--- src/tests/reverseString.test.js
+++ src/tests/reverseString.test.js
@@ -1,3 +1,10 @@
+import { experiments } from "webpack";
+import { reverseString } from "../scripts/reverseString";
+
 test("abcd returns dcba", () => {
   expect(reverseString("abcd")).toBe("dcba");
+});
+
+test("abijgis returns sigjiba", () => {
+  expect(reverseString("abijgis")).toBe("sigjiba");
 });
